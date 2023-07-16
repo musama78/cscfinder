bcc324694791b501466918daf7cf1b81186ff876
usama <muhammadusama7@yahoo.com>
2023-07-16 09:47:21 +0000

    54

--- mongodb-init.sh
+++ mongodb-init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Find the container name containing "mongodb"
-container_name=$(docker ps --format '{{.Names}}' | grep 'mongodb')
-echo  "************* GOING INSIDE CONTAINER *************"
-# Execute the command within the MongoDB container
-docker exec -it "$container_name" mongo <<EOF
+echo  "************* GOING INSIDE MONGODB *************"
+
+mongosh <<EOF
 use $MONGODB_DATABASE
 db.createUser({
   user: "$MONGODB_USERNAME",
   pwd: "$MONGODB_PASSWORD",
-  roles: [{ role: "root", db: "$MONGODB_DATABASE" }]
+  roles: [{ role: "readWrite", db: "$MONGODB_DATABASE" }]
 })
 EOF
-echo  "**************************"
+echo  "************ DONE **************"
+
