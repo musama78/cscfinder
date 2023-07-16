4e8b4c3191e26952c61009da059cd0f7462d6dd7
usama <muhammadusama7@yahoo.com>
2023-07-16 08:05:50 +0000

    48

--- mongo-init.js/init-mongo.js
+++ mongo-init.js/init-mongo.js
@@ -7,12 +7,28 @@ print("***************** Initializing MongoDB container *****************");
 #    { role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }
 #  ]
 #});
-db = db.getSiblingDB(MONGODB_DATABASE);
-db.createUser({
-  user: env.MONGODB_USERNAME,
-  pwd: env.MONGODB_PASSWORD,
+#############
+############
+#db = db.getSiblingDB(MONGODB_DATABASE);
+#db.createUser({
+#  user: env.MONGODB_USERNAME,
+#  pwd: env.MONGODB_PASSWORD,
+#  roles: [
+#    { role: 'readWrite', db: MONGODB_DATABASE }
+#  ]
+#});
+#############
+############
+const myusername = process.env.MONGODB_USERNAME;
+const mypassword = process.env.MONGODB_PASSWORD;
+const mydatabase = process.env.MONGODB_DATABASE;
+
+db.getSiblingDB(databaseName).createUser({
+  user: myusername,
+  pwd: mypassword,
   roles: [
-    { role: 'readWrite', db: MONGODB_DATABASE }
+    { role: "readWrite", db: mydatabase }
   ]
 });
+
 print("***************** MongoDB container Initialization Complete *****************");
